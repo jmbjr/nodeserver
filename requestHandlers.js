319eb27a2bdbbf206c07d1ca0fa4f8a9559bf1df
jmbjr <boylejm@gmail.com>
2014-06-16 02:05:24 -0400

    fix a typo

--- requestHandlers.js
+++ requestHandlers.js
@@ -16,7 +16,7 @@ function start(response) {
     '</body>' +
     '</html>';
 
-    response.writeHead(200, {200, {"Content-Type": "text/html"});
+    response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
     response.end();
 }
