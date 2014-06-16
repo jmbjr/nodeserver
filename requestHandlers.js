edeb2309cf52edf3c2c1b46cb0b1c0835dd426e3
jmbjr <boylejm@gmail.com>
2014-06-16 03:33:42 -0400

    remove postData and add file upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,6 @@
 var querystring = require("querystring"),
-    fs = require("fs");
+    fs = require("fs"),
+    formidable = require("formidable");
 
 function start(response) {
   console.log("Request handler 'start' was called.");
@@ -10,9 +11,10 @@ function start(response) {
     'charset=UTF-8" />' +
     '</head>' +
     '<body>' + 
-    '<form action="/upload" method="post">' +
-    '<textarea name="text" rows="20" cols="60"></textarea>' +
-    '<input type="submit" value="Submit text" />' +
+    '<form action="/upload" enctype="multipart/form-data" ' +
+    'method="post">' +
+    '<input type="file" name="upload" multiple="multiple">' +
+    '<input type="submit" value="Upload file" />' +
     '</form>' +
     '</body>' +
     '</html>';
@@ -22,12 +24,27 @@ function start(response) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("Request handler 'upload' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Uploaded Text: " + 
-  querystring.parse(postData).text);
-  response.end();
+
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log("parsing done");
+
+    /* Possible error on WIndows systems:
+      tried to rename to an already existing file */
+    fs.rename(files.upload.path, "./tmp/test.png", function(error) {
+      if (error) {
+        fs.unlink("./tmp/test.png");
+        fs.rename(files.upload.path, "./tmp/test.png");
+      }
+    });
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image: <br/>");
+    response.write("<img src='/show' />"); 
+    response.end();
+  });
 }
 
 function show(response) {
